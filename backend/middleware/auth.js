0876b162d35fd0348706ad039505451b0073e90d
Fera 13 <fera6055@gmail.com>
2023-05-19 20:02:15 +0200

    new deploy try

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,28 +1,30 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 function checkRole(role) {
-    return function(req, res, next) {
-      const userRole = req.user.role
-      if (!role.includes(userRole)) {
-        return res.status(402).json({message: 'Forbidden you dont have access to that resource'})
-      } 
-      next()
+  return function (req, res, next) {
+    const userRole = req.user.role;
+    if (!role.includes(userRole)) {
+      return res
+        .status(402)
+        .json({ message: "Forbidden you dont have access to that resource" });
     }
+    next();
+  };
 }
 
 function authenticateUser(req, res, next) {
-    const { cookies } = req
-    const token = cookies.accessToken
-
-    if(token == null) return res.sendStatus(403)
-    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
-      if(err) return res.sendStatus(403)
-      req.user = user
-      next()
-    })
+  const { cookies } = req;
+  const token = cookies.accessToken;
+  console.log(token);
+  if (token == null) return res.sendStatus(403);
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
 module.exports = {
-    checkRole,
-    authenticateUser
-}
+  checkRole,
+  authenticateUser,
+};
